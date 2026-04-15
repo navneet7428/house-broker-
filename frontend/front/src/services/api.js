9ece6f8e05f647f382e651c8c4be028227e75dd8
navneet gupta <guptanavneet2004@gmail.com>
2026-04-16 02:20:51 +0530

    Updated API URL

--- frontend/front/src/services/api.js
+++ frontend/front/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001/api",
+  baseURL: "https://house-broker-backend.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
